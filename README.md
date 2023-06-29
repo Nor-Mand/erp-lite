f84c943634ed1b9bb6cff14cf8a709c943c66426
Normand <normandlaredo@gmail.com>
2023-06-30 00:55:20 +0100

    [ADD] adding financial dashboard to home page

--- README.md
+++ README.md
@@ -26,6 +26,7 @@ Enterprise Resource Planning using Django Framework
 
 ### Login
 ![alt text](https://github.com/Nor-Mand/erp-lite/blob/main/images/login.png)
+![alt text](https://github.com/Nor-Mand/erp-lite/blob/main/images/dashboard.png)
 ![alt text](https://github.com/Nor-Mand/erp-lite/blob/main/images/picture_2.png)
 ![alt text](https://github.com/Nor-Mand/erp-lite/blob/main/images/picture_3.png)
 ![alt text](https://github.com/Nor-Mand/erp-lite/blob/main/images/picture_4.png)
---
+++ home/templates/charts/sales/sales.js
@@ -0,0 +1,48 @@
+var ctx = document.getElementById("barChartSales").getContext("2d");
+var labels = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Ago",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+var barChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: labels,
+    datasets: [
+      {
+        data: [69, 39, 69, 12, 49, 63, 24, 100, 69, 80, 30, 85],
+        backgroundColor: ["rgba(1, 209, 255)"],
+        borderColor: ["rgba(54, 162, 235, 0.2)"],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    scales: {
+      x: {
+        ticks: {
+          color: "#F3F4F6",
+        },
+      },
+      y: {
+        ticks: {
+          color: "#F3F4F6",
+        },
+      },
+    },
+  },
+});
